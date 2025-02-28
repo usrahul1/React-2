aa14094b9740fa796e4a818bd9644cf80519676e
Sai Rahul <sairahulurumu@gmail.com>
2025-02-28 22:30:03 +0530

    adding everything

---
+++ src/config/gemini.js
@@ -0,0 +1,35 @@
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const apiKey = "AIzaSyD-tE_XcFSxyTbKn1BdxPQ0sdWaQtGrPng"
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    return result.response.text()
+  }
+  
+  export default run;
